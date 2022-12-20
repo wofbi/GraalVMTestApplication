ebce997bd238fb4d5870dc50c714e6b4326816e2
wofbi <wofbi1@outlook.com>
2022-12-20 10:20:37 +0800

    first commit

---
+++ src/main/java/com/example/graalvmtest/GraalVMTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.graalvmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalVMTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalVMTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graalvmtest/GraalVMTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalvmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalVMTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
